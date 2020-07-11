f144423fbf473025da2e0749c1bc28a00aaa7345
DominikLewandowski <ddlewandowski@wp.pl>
2020-07-11 17:12:14 +0200

    Implementacja drugiej sciezki blokow

--- sources_1/new/block_generator.v
+++ sources_1/new/block_generator.v
@@ -25,7 +25,7 @@ module block_generator(
   input wire rst, 
   input wire generate_map,
   output reg [0:6] layer_map,
-  output reg [0:6] block_type,
+  output wire [0:6] block_type,
   output reg load_layer,
   output reg map_ready
 );
@@ -46,18 +46,27 @@ module block_generator(
   wire [31:0] pseudo_number = (z1 ^ z2 ^ z3 ^ z4);
   
   reg [2:0] state, state_nxt = S_START;
-  reg [0:6] block_type_nxt, layer_map_nxt; 
+  reg [0:6] main_track, main_track_nxt, layer_map_nxt; 
+  reg [0:6] second_track, second_track_nxt;
   reg load_layer_nxt, map_ready_nxt;
-  reg direction;
+  reg direction_main, direction_second;
+  reg [3:0] block_counter, block_counter_nxt;
+  reg track_en, track_en_nxt;
   
-  wire direction_nxt = ( ^pseudo_number[31:16] == 1'b1 ) ? LEFT : RIGHT;
+  wire direction_main_nxt = ( ^pseudo_number[31:16] == 1'b1 ) ? LEFT : RIGHT;
+  wire direction_second_nxt = ( ^pseudo_number[15:0] == 1'b0 ) ? LEFT : RIGHT;
+  
+  assign block_type = main_track | second_track;
     
   always @(*) begin
     state_nxt = state;
     layer_map_nxt = layer_map;
-    block_type_nxt = block_type;
+    main_track_nxt = main_track;
+    second_track_nxt = second_track;
+    block_counter_nxt = block_counter;
     load_layer_nxt = 0;
     map_ready_nxt = 0;
+    track_en_nxt = track_en;
     
     case(state)
       S_START: 
@@ -65,30 +74,32 @@ module block_generator(
       
       S_LAYER_1: begin
         layer_map_nxt = 7'b0001000;
-        block_type_nxt = 7'b0001000;
+        main_track_nxt = 7'b0001000;
         load_layer_nxt = 1;
         state_nxt = S_LAYER_2;
       end
       
       S_LAYER_2: begin
         layer_map_nxt = 7'b1010101;
-        block_type_nxt = ( direction == LEFT ) ? ( block_type << 1 ) : ( block_type >> 1 );
+        main_track_nxt = ( direction_main == LEFT ) ? ( main_track << 1 ) : ( main_track >> 1 );
         load_layer_nxt = 1;
         state_nxt = S_LAYER_3;
       end
       
       S_LAYER_3: begin
         layer_map_nxt = layer_map ^ 7'b1111111;
-        block_type_nxt = ( direction == LEFT ) ? ( block_type << 1 ) : ( block_type >> 1 );
+        main_track_nxt = ( direction_main == LEFT ) ? ( main_track << 1 ) : ( main_track >> 1 );
         load_layer_nxt = 1;
         state_nxt = S_LAYER_4;
       end
       
       S_LAYER_4: begin
         layer_map_nxt = layer_map ^ 7'b1111111;
-        block_type_nxt = ( direction == LEFT ) ? ( block_type << 1 ) : ( block_type >> 1 );
+        main_track_nxt = ( direction_main == LEFT ) ? ( main_track << 1 ) : ( main_track >> 1 );
         load_layer_nxt = 1;
         map_ready_nxt = 1;
+        block_counter_nxt = 0;
+        track_en_nxt = 0;
         state_nxt = S_IDLE;
       end
       
@@ -97,9 +108,34 @@ module block_generator(
       
       S_GENERATE: begin
         layer_map_nxt = layer_map ^ 7'b1111111;
-        if( block_type == 7'b1000000 ) block_type_nxt = block_type >> 1;
-        else if( block_type == 7'b0000001 ) block_type_nxt = block_type << 1;
-        else block_type_nxt = ( direction == LEFT ) ? ( block_type << 1 ) : ( block_type >> 1 );
+        if( main_track == 7'b1000000 ) begin
+          main_track_nxt = main_track >> 1;
+          second_track_nxt = ( track_en == 1 ) ? second_track : (main_track >> 1);
+        end
+        else if( main_track == 7'b0000001 ) begin
+          main_track_nxt = main_track << 1;
+          second_track_nxt = ( track_en == 1 ) ? second_track : (main_track << 1);
+        end  
+        else begin
+          main_track_nxt = ( direction_main == LEFT ) ? ( main_track << 1 ) : ( main_track >> 1 );
+          second_track_nxt = ( track_en == 1 ) ? second_track : (( direction_main == LEFT ) ? ( main_track << 1 ) : ( main_track >> 1 ));
+        end  
+        
+        if( (track_en == 0) && (pseudo_number[3:0] < 4'hA) ) begin
+          track_en_nxt = 1'b1;
+          block_counter_nxt = pseudo_number[7:4];
+        end    
+              
+        if( track_en == 1'b1 ) begin
+          block_counter_nxt = block_counter - 1;
+          if( (block_counter == 4'h0) && (^block_type == 1'b1) ) begin 
+            second_track_nxt = 7'b0000000;
+            track_en_nxt = 1'b0;
+          end
+          else if( second_track == 7'b1000000 ) second_track_nxt = second_track >> 1;
+          else if( second_track == 7'b0000001 ) second_track_nxt = second_track << 1;
+          else second_track_nxt = ( direction_second == LEFT ) ? ( second_track << 1 ) : ( second_track >> 1 );
+        end
         state_nxt = S_IDLE;
       end
         
@@ -111,18 +147,26 @@ module block_generator(
     if(rst) begin
       state <= S_START;
       layer_map <= 7'b0000000;
-      block_type <= 7'b0000000;
+      main_track <= 7'b0000000;
+      second_track <= 7'b0000000;
       load_layer <= 1'b0;
       map_ready <= 1'b0;
-      direction <= 1'b0;
+      direction_main <= 1'b0;
+      direction_second <= 1'b0;
+      block_counter <= 4'b0;
+      track_en <= 1'b0;
     end 
     else begin
       state <= state_nxt;
       layer_map <= layer_map_nxt;
-      block_type <= block_type_nxt;
+      main_track <= main_track_nxt;
+      second_track <= second_track_nxt;
       load_layer <= load_layer_nxt;
       map_ready <= map_ready_nxt;
-      direction <= direction_nxt;
+      direction_main <= direction_main_nxt;
+      direction_second <= direction_second_nxt;
+      block_counter <= block_counter_nxt;
+      track_en <= track_en_nxt;
     end
   end
   
@@ -134,6 +178,7 @@ module block_generator(
   end
   
   always @( posedge clk ) 
-    {z1, z2, z3, z4} <= {z1_nxt, z2_nxt, z3_nxt, z4_nxt};
-  
+    if(rst) {z1, z2, z3, z4} <= {SEED, SEED, SEED, SEED};
+    else {z1, z2, z3, z4} <= {z1_nxt, z2_nxt, z3_nxt, z4_nxt};
+    
 endmodule
